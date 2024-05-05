716e9a7e4a327d22afa522257c8b8a0ea08d4381
Codename;0 <139770129+codename0og@users.noreply.github.com>
2024-05-06 00:48:40 +0200

    Add files via upload

---
+++ requirements/realtime-voice-changer/install requirements for realtime voice-changer ( NVIDIA-RTX30XX ).bat
@@ -0,0 +1,17 @@
+@echo off
+REM Change to the "reclists-voice-changer" directory if it exists
+if exist "reclists-voice-changer" (
+    cd /d "reclists-voice-changer"
+)
+
+REM Install torch, torchvision, and torchaudio cuda-ver-cu117
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
+
+REM Install packages listed in the requirements file for voice-changer nvidia-rtx30xx-cu117
+pip install -r requirements-win-for-realtime_vc_gui-cuda.txt
+
+REM Add error handling if needed
+if %errorlevel% neq 0 (
+    echo An error occurred during installation.
+    exit /b %errorlevel%
+)
